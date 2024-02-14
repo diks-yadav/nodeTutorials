5476bcde5b9b801122e08f9aff5f7ed84936f0ba
techie777 <techsculptor8@gmail.com>
2024-02-14 09:55:48 +0530

    added multer

--- expressproject/server.js
+++ expressproject/server.js
@@ -1,23 +1,32 @@
-const express = require('express');
-const app =express();
-const port=8000;
-app.get('/', function(req,res){
-       res.send({message:"root api calling",status:1});
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const upload = multer();
+const port = 8000;
+app.get("/", function (req, res) {
+  res.send({ message: "root api calling", status: 1 });
 });
 
-app.get('/home', function(req,res){
-    res.send({message:"home api calling",status:1});
+app.get("/home", function (req, res) {
+  res.send({ message: "home api calling", status: 1 });
 });
-app.get('/contact', function(req,res){
-    res.send({message:"contact api calling",status:1});
+app.get("/contact", function (req, res) {
+  res.send({ message: "contact api calling", status: 1 });
 });
-app.get('/about', function(req,res){
-    res.send({message:"about api calling",status:1});
+app.get("/about", function (req, res) {
+  res.send({ message: "about api calling", status: 1 });
 });
-app.post('/register', function(req,res){
-    var amount =500
-    res.send({message:"register api calling",status:1,amount:amount});
+app.post("/register", upload.single(), function (req, res) {
+  console.log("req.body", req.body);
+  var amount = req.body.amount;
+  var type = req.body.type;
+  if (type == 1) {
+    var totalAmount = +amount;
+  } else {
+    var totalAmount = +amount * 80;
+  }
+  res.send({ message: "register api calling", status: 1, amount: totalAmount });
 });
-app.listen(port,function(){
-    console.log(`server listeninin on http://localhost:${port}`);
-})
+app.listen(port, function () {
+  console.log(`server listeninin on http://localhost:${port}`);
+});
